d4c3d9cc4dd2a26bf2ea0984e17c261c45eac541
srvsingh53 <ch21btech11026@iith.ac.in>
2025-01-23 21:30:19 +0530

    Implemented Outward Sync

--- src/services/kafka_consumer.py
+++ src/services/kafka_consumer.py
@@ -3,16 +3,27 @@ import json
 from src.services.stripe_service import create_customer
 from src.config.settings import KAFKA_BROKER, KAFKA_TOPIC
 
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 consumer = KafkaConsumer(
     KAFKA_TOPIC,
     bootstrap_servers=KAFKA_BROKER,
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+    value_deserializer=lambda x: json.loads(x.decode('utf-8')),
+    auto_offset_reset='earliest',
+    enable_auto_commit=True
 )
 
 def consume_events():
-    print("Listening for customer events...")
+    logger.info("Consumer started, listening for events...")
     for message in consumer:
         event_data = message.value
+        logger.info(f"Received event: {event_data}")
+        
         if event_data['event'] == 'customer_created':
-            create_customer(event_data['data']['name'], event_data['data']['email'])
-        print(f"Processed event: {event_data}")
+            stripe_customer_id = create_customer(event_data['data']['name'], event_data['data']['email'])
+            if stripe_customer_id:
+                logger.info(f"Customer created in Stripe with ID: {stripe_customer_id}")
+            else:
+                logger.error("Failed to create customer in Stripe.")
